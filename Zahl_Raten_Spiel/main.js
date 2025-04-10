63bbbcc758d3831f9dd92f53cef467ebe86e99fc
Schleiml <schleiml.ende@gmail.com>
2025-04-10 14:43:29 +0200

    Zahlen Raten spiel hinzugefügt

---
+++ Zahl_Raten_Spiel/main.js
@@ -0,0 +1,15 @@
+let ZahlR = Math.floor(Math.random() * 10) + 1;
+console.log(ZahlR);
+
+function Input_saver() {
+    let Inhalt = document.getElementById("Input").value;
+    if (Inhalt > ZahlR) {
+        document.getElementById("Punkte").innerHTML = "Zu Groß!"
+    }
+    else if (Inhalt < ZahlR) {
+        document.getElementById("Punkte").innerHTML = "Zu Klein!"
+    }
+    else {
+        document.getElementById("Punkte").innerHTML = "Richtig!"
+    }
+}
