398d0c7dd8f24671401577bc2b725f3213ff8b4b
Schleiml <schleiml.ende@gmail.com>
2025-04-11 11:26:46 +0200

    eigene Auswahl geaddet und Bot auswhl geaddet

--- Schere_Stein_Papier_Spiel/main.js
+++ Schere_Stein_Papier_Spiel/main.js
@@ -1,7 +1,28 @@
-let Schere = 1002;
-let Stein = 1003;
-let Papier = 1004;
+let Auswahl;
 
-function SchereisPressed() {
-    
+function Schere_isPressed() {
+    Auswahl = "Schere";
+    document.getElementById("Schere").style.backgroundColor = "green";
+    console.log("Meine Auswahl: " + Auswahl);
+}
+function Stein_isPressed() {
+    Auswahl = "Stein";
+    document.getElementById("Stein").style.backgroundColor = "green";
+    console.log("Meine Auswahl: " + Auswahl);
+}
+function Papier_isPressed() {
+    Auswahl = "Papier";
+    document.getElementById("Papier").style.backgroundColor = "green";
+    console.log("Meine Auswahl: " + Auswahl);
+}
+function Choosing() {
+    let PC_Auswahl = Math.floor(Math.random() * 3) + 1;
+    if (PC_Auswahl == 1) {
+        PC_Auswahl = "Schere";
+    } else if (PC_Auswahl == 2) {
+        PC_Auswahl = "Stein";
+    } else if (PC_Auswahl == 3) {
+        PC_Auswahl = "Papier";
+    }
+    console.log("Bot Auswahl: " + PC_Auswahl);
 }
